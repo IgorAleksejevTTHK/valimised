ed1707f5c62852789d6bc32159c0b33f38f20bac
IgorAleksejevTTHK <igoralekseje@gmail.com>
2026-01-05 12:57:47 +0200

    function on täiendatud

--- funktsioonid.php
+++ funktsioonid.php
@@ -8,7 +8,6 @@ function lisapunkt($id){
         $paring = $yhendus->prepare("UPDATE valimised SET punktid=punktid+1 WHERE id=?");
         $paring->bind_param("i", $id);
         $paring->execute();
-        header("Location: $_SERVER[PHP_SELF]"); //adressiriba puhastab päring ja jääb faili nimi
         $yhendus->close();
 
 }
